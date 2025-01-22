b9e3f73af89c2e35f36215b156c3ff28b708e5f4
User <User@DESKTOP-TJQPIB5>
2025-01-23 00:28:37 +0100

    Dodaj pliki projektów.

---
+++ OnlineCookbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineCookbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
